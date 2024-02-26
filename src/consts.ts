1ff82fcf57092f08eb8916902b6ff36af86d08bd
Miguel Ángel Ramírez Ruiz <mugui098@gmail.com>
2024-02-26 21:09:44 +0100

    changes: tweak theme, header, footer and home page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "DONTKNOW.DEV";
+export const SITE_SEO_TITLE = "Learn with me! - dontknow.dev";
+export const SITE_SEO_DESCRIPTION =
+  "Join dontknow.dev to read about computers, software, and web development. Explore posts where I write about topics I'm learning about, so we can learn together!";
+
+export const TWITTER_URL = "https://twitter.com/miguelo0098";
+export const GITHUB_URL = "https://github.com/miguelo0098";
