69c98b997a953e6c23b686712916cb5cd02947fb
Miguel Ángel Ramírez Ruiz <mugui098@gmail.com>
2024-03-04 20:26:24 +0100

    chores: add netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://dontknow.dev",
   integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
